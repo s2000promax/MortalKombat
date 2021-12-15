b49e56d88d46c0b35bd467613d24a5c65a144628
Stan <s2000ProMax@gmail.com>
2021-12-16 00:13:36 +0300

    homework-5

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const HIT = {
+  head: 30,
+  body: 25,
+  foot: 20,
+};
+const ATTACK = ["head", "body", "foot"];
+
 const player1 = {
   player: 1,
   name: "Subzero",
@@ -25,29 +32,34 @@ const player2 = {
   elHP: elHP,
 };
 
-const $arenas = document.querySelector(".arenas");
-const $randomButton = document.querySelector(".button");
-const $restartButton = document.querySelector(".control")
-
 //Функция создает элемент HTML
 function createElement(tag, className) {
   $tag = document.createElement(tag);
   if (className) {
     $tag.classList.add(className);
   }
+
   return $tag;
 }
+
+const $arenas = document.querySelector(".arenas");
+const $formControl = document.querySelector(".control");
+const $winTitle = createElement("div", "loseTitle");
+
 
 //Функция создает кнопку перезагрузки
 function createReloadButton() {
   const $div = createElement("div", "reloadWrap");
   const $btn = createElement("button", "button");
+
   $btn.innerText = "Restart";
+  
+  $div.appendChild($btn);
+
   $btn.addEventListener("click", function () {
     window.location.reload();
     // console.log("Restart");
   });
-  $div.appendChild($btn);
 
   return $div;
 }
@@ -55,35 +67,58 @@ function createReloadButton() {
 //Функция создает игрока
 function createPlayer(playerObject) {
   const $player = createElement("div", `player${playerObject.player}`);
-
   const $progressbar = createElement("div", "progressbar");
-  $player.appendChild($progressbar);
-
   const $life = createElement("div", "life");
-  $life.style.width = `${playerObject.hp}%`;
-  $progressbar.appendChild($life);
-
   const $name = createElement("div", "name");
-  $name.innerText = playerObject.name;
-  $progressbar.appendChild($name);
-
   const $character = createElement("div", "character");
   const $img = createElement("img");
+  
+  $life.style.width = `${playerObject.hp}%`;
+  $name.innerText = playerObject.name;
   $img.src = playerObject.img;
+  
+  $player.appendChild($progressbar);
+  $progressbar.appendChild($life);
+  $progressbar.appendChild($name);
   $character.appendChild($img);
   $player.appendChild($character);
 
   return $player;
 }
 
-//Функция возврщает div победителя или ничьи
-function playerWin(name) {
-  const $winTitle = createElement("div", "winTitle");
-  if (name) {
-    $winTitle.innerText = name + " wins";
-  } else $winTitle.innerText = "DEAD BOTH";
+//Функция управляет отоборажением кнопок
+function buttonRender(bool, control) {
+  if (bool) {
+    //$fightButton.disabled = true; //Блокируем кнопку Random
+    // $fightButton.style = "display: none";
 
-  return $winTitle;
+    //Деактивируем элементы формы
+    if (control) {
+      for (item of control) {
+        if (item.tagName === 'INPUT' || item.tagName === 'BUTTON') { item.disabled = true }
+      }
+    }
+       
+    $arenas.appendChild(createReloadButton()); //Рендерим кнопку Restart
+  }
+}
+
+//Функция возврщает div победителя или ничьи
+function playerWin(className, playerName) {
+  if (playerName) {
+    className.style = 'font-size: 36px'
+    className.innerText = playerName + " wins";
+  } else className.innerText = "DEAD BOTH";
+
+  return className;
+}
+
+//Функция трансляция/комментарии боя
+function createComment(className, text,  player) {
+  className.style = 'font-size: 20px'
+  className.innerText = text;
+  
+  return className;
 }
 
 //Функция генерирует случайное число в диапазоне 1...number
@@ -93,10 +128,10 @@ function getRandom(number) {
   } else return 20;
 }
 
-//Функция уменьшает HP у объекта (игрока) на величину attack
-function changeHP(attack) {
-  if (this.hp - attack > 0) {
-    this.hp -= attack;
+//Функция уменьшает HP у объекта (игрока) на величину damage
+function changeHP(damage) {
+  if (this.hp - damage > 0) {
+    this.hp -= damage;
   } else this.hp = 0;
 }
 
@@ -110,34 +145,100 @@ function elHP() {
 //Функция отрисовывает "жизнь" у определенного объекта (игрока)
 function renderHP() {
   //const $playerLife = elHP.call(this); //Функции elHP передаем объект player через метод call
- // $playerLife.style.width = `${this.hp}%`;
+  // $playerLife.style.width = `${this.hp}%`;
 
   //return $playerLife;
-  return this.elHP().style.width = `${this.hp}%`;
+  return (this.elHP().style.width = `${this.hp}%`);
 }
 
-//Функция управляет отоборажением кнопок
-function buttonRender(bool) {
-  if (bool) {
-    //$randomButton.disabled = true; //Блокируем кнопку Random
-    $randomButton.style = "display: none";
+//Функция - удар противника
+function enemyAttack() {
+  const hit = ATTACK[getRandom(3) - 1];
+  const defence = ATTACK[getRandom(3) - 1];
 
-    $restartButton.appendChild(createReloadButton()); //Рендерим кнопку Restart
+  return {
+    value: getRandom(HIT[hit]),
+    hit,
+    defence,
+  };
+}
+
+//Функция - удар игрока (обработка формы)
+function playerAttack(formControl) {
+  const attack = {};
+  if (formControl) {
+    for (let item of formControl) {
+      if (item.checked && item.name === "hit") {
+        attack.value = getRandom(HIT[item.value]);
+        attack.hit = item.value;
+      }
+      if (item.checked && item.name === "defence") {
+        attack.defence = item.value;
+      }
+      //Обнуление контролов
+      item.checked = false;
+    }
+  } else
+    attack = {
+      value: 0,
+      hit: "",
+      defence: "",
+    };
+
+  return attack;
+}
+
+//Fight
+function fight(player1, player2) {
+  const player = playerAttack($formControl); //Условно играет за объект player1
+  const enemy = enemyAttack(); //Условно играет за объект player2
+
+  // console.log('Player1', player)
+  // console.log('Player2', enemy)
+
+  //Определяем результат раунда
+  if (player.hit != enemy.defence && enemy.hit != player.defence) {
+    player1.changeHP(enemy.value);//Отнимаем жизни у player1 на величину удара противника
+    player2.changeHP(player.value);//Отнимаем жизни у player2 на величину удара противника
+    //Комментируем раунд
+    $arenas.appendChild(createComment(
+      $winTitle ,`${player1.name} damaged | ${player2.name} damaged`
+      ));
   }
+
+  if (player.hit != enemy.defence && enemy.hit === player.defence) {
+    player2.changeHP(player.value);
+    $arenas.appendChild(createComment(
+      $winTitle ,`${player1.name} blocked | ${player2.name} damaged`
+      ));
+  }  
+
+  if (player.hit === enemy.defence && enemy.hit != player.defence) {
+    player1.changeHP(enemy.value);
+    $arenas.appendChild(createComment(
+      $winTitle ,`${player1.name} damaged | ${player2.name} blocked`
+      ));
+  }  
+
+  if (player.hit === enemy.defence && enemy.hit === player.defence) {
+    $arenas.appendChild(createComment(
+      $winTitle ,`${player1.name} blocked | ${player2.name} blocked`
+      ));
+  }   
 }
 
 //Функция следит за результатом боя, определяет победителя
 function checkResult(player1, player2) {
   if (player1.hp <= 0 || player2.hp <= 0) {
-    buttonRender(true); //Передаем флаг для отрисовки/блокировки кнопок
+    buttonRender(true, $formControl); //Передаем флаг для отрисовки/блокировки кнопок
   }
   //Определяем победителя
   if (player1.hp === 0 && player2.hp === 0) {
     $arenas.appendChild(playerWin());
   } else if (player1.hp === 0 && player2.hp > 0) {
-    $arenas.appendChild(playerWin(player2.name));
+    $arenas.appendChild(playerWin($winTitle ,player2.name));
   } else if (player2.hp === 0 && player1.hp > 0) {
-    $arenas.appendChild(playerWin(player1.name));
+    $arenas.appendChild(playerWin($winTitle ,player1.name));
   }
 }
 
@@ -146,12 +247,13 @@ $arenas.appendChild(createPlayer(player1));
 $arenas.appendChild(createPlayer(player2));
 
 //Генерируем бой
-$randomButton.addEventListener("click", function () {
-  player1.changeHP(getRandom(20));
-  player2.changeHP(getRandom(20));
+$formControl.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  fight(player1, player2)
+
   player1.renderHP();
   player2.renderHP();
+
   checkResult(player1, player2);
-  // console.log(`player${1} ` + player1.hp + "%");
-  // console.log(`player${2} ` + player2.hp + "%");
 });
